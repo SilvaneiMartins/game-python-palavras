cb34954177e08bf269ac751e1b1fd5a43772cdd7
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-02-16 16:01:45 -0400

    feat: setting server one

---
+++ server/round.py
@@ -0,0 +1,73 @@
+'''
+  Represents a round of the game, storing things like
+  word, time, skips, drawing player and more.
+'''
+import time as t
+from _thread import *
+from game import Game
+from chat import Chat
+
+
+class Round(object):
+
+    def __init__(self, word, player_drawing, players):
+        """
+        init object
+        :param word: str
+        :param player_drawing: Player
+        :param players: Player[]
+		"""
+
+        self.word = word
+        self.player_drawing = player_drawing
+        self.player_gressed - []
+        self.skips = 0
+        self.player_scores = {players: 0 for player in players}
+        self.time = 75
+		self.chat = Chat(self)
+        start_new_thread(self.time_thread, ())
+
+    def time_thread(self):
+        """
+        Runs in thread to keep track of time
+        :return: None
+        """
+        while self.time > 0:
+            t.sleep(1)
+            self.time -= 1
+
+        self.end_round("Time is up!")
+
+    def guess(self, player, wrd):
+        """
+        :returns bool if player got guess correct
+        :param player: Player
+        :param wrd: str
+        :return: bool
+"""
+
+        correct = wrd == self.word
+        if correct:
+            self.player_gressed.append(player)
+            # TODO: implement scoring system here
+
+    def player_left(self, player):
+        """
+removes player that left from scores and list
+:param player: Player
+:return: None
+"""
+
+        # might not be able to use player as key in dict
+        if player in player_scores:
+            del self.player_scores[player]
+
+        if player in self.player_gressed:
+            self.player_gressed.remove(player)
+
+        if player == self.player_drawing:
+            self.end_round("Drawing player leaves")
+
+    def end_round(self, msg):
+        # TODO: implement end_round functionallity
+        pass
